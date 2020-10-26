5427e1ba0e2910494fb40ff7693d77f811064bba
Jeadness <68141999+Jeadness@users.noreply.github.com>
2020-10-26 11:22:40 +0300

    Add files via upload

---
+++ Simple Web Browser/Simple Web Browser/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Simple_Web_Browser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.buttonForward = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Image = global::Simple_Web_Browser.Properties.Resources.magnifying_glass;
+            this.buttonGo.Location = new System.Drawing.Point(1218, 9);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(49, 53);
+            this.buttonGo.TabIndex = 2;
+            this.buttonGo.Text = " ";
+            this.buttonGo.UseVisualStyleBackColor = false;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // buttonForward
+            // 
+            this.buttonForward.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.buttonForward.Image = global::Simple_Web_Browser.Properties.Resources.iconForward;
+            this.buttonForward.Location = new System.Drawing.Point(72, -2);
+            this.buttonForward.Name = "buttonForward";
+            this.buttonForward.Size = new System.Drawing.Size(64, 71);
+            this.buttonForward.TabIndex = 1;
+            this.buttonForward.Text = " ";
+            this.buttonForward.UseVisualStyleBackColor = false;
+            this.buttonForward.Click += new System.EventHandler(this.buttonForward_Click);
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.buttonBack.Image = global::Simple_Web_Browser.Properties.Resources.iconBack;
+            this.buttonBack.Location = new System.Drawing.Point(2, -2);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(64, 71);
+            this.buttonBack.TabIndex = 0;
+            this.buttonBack.Text = " ";
+            this.buttonBack.UseVisualStyleBackColor = false;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // urlBox
+            // 
+            this.urlBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.urlBox.Location = new System.Drawing.Point(157, 40);
+            this.urlBox.Name = "urlBox";
+            this.urlBox.Size = new System.Drawing.Size(1055, 22);
+            this.urlBox.TabIndex = 3;
+            this.urlBox.Text = "\r\n";
+            // 
+            // browser
+            // 
+            this.browser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.browser.Location = new System.Drawing.Point(2, 75);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(1276, 656);
+            this.browser.TabIndex = 4;
+            this.browser.Url = new System.Uri("http://google.com", System.UriKind.Absolute);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Minecraft", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(488, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 28);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Simple Web Browser";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1279, 730);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.browser);
+            this.Controls.Add(this.urlBox);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.buttonForward);
+            this.Controls.Add(this.buttonBack);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Simple Web Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonForward;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.TextBox urlBox;
+        private System.Windows.Forms.WebBrowser browser;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Simple Web Browser/Simple Web Browser/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Web_Browser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            browser.GoBack();
+        }
+
+        private void buttonForward_Click(object sender, EventArgs e)
+        {
+            browser.GoForward();
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+            browser.Navigate(urlBox.Text);
+        }
+    }
+}
